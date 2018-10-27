5a05670318c35b11396eb7a406739b537e6bd8ad
Meazer <meazer.m@gmail.com>
2018-10-27 16:02:00 +0200

    fix axios post

--- servers/pagesServer.js
+++ servers/pagesServer.js
@@ -8,9 +8,9 @@ const path = require('path');
 const axios = require("axios");
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,"views"));
-app.use("/public", express.static(path.join(__dirname,"public")));
-app.use("/script", express.static(path.join(__dirname,"views","script")));
+app.set("views", path.join(__dirname, "views"));
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/script", express.static(path.join(__dirname, "views", "script")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -49,44 +49,54 @@ app.get("/cities", (req, res) => {
   res.send(cities);
 });
 
-// axios
-//   .post("localhost:5000/api/weather", {
-//     locations: [{ lat: "-38.36", lng: "-22.73", timestamp: "1539969000" }]
-//   })
-//   .then(res => console.log(res))
-//   .catch(err => console.log(err));
+
 
 app.post("/weather", (req, res) => {
-  // let lng = req.body.longitude;
-  // let lat = req.body.latitude;
-  // let timestamp = req.body.timestamp;
+  let lng = req.body.longitude;
+  let lat = req.body.latitude;
+  let timestamp = req.body.timestamp;
 
-  const city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  request(url, function(err, response, body) {
-    if (err) {
-      res.render("weather", {
-        weather: null,
-        error: "Error, please try again"
-      });
-    } else {
-      let weather = JSON.parse(body);
-      if (weather.main === undefined) {
-        res.render("weather", {
-          weather: null,
-          error: "Error, please try again"
-        });
-      } else {
-        let weatherText = weather.main.temp;
-        let city = weather.name;
-        let description = weather.weather[0].description;
-        res.render("weather", {
-          weather: { weatherText, city, description },
-          error: null
-        });
-      }
-    }
-  });
+
+  axios
+    .post("http://localhost:5000/api/weather", { locations: [{ "lat": 53, "lng": 7, "timestamp": 1541554733 }] }
+    )
+    .then(res => handleData(res.data.result))
+    .catch(err => console.log('err', err));
+
+  const handleData = (data) => {
+    console.log(data);
+    res.render("weather", {
+      data
+    });
+  }
+
+
+  // const city = req.body.city;
+  // let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  // request(url, function(err, response, body) {
+  //   if (err) {
+  //     res.render("weather", {
+  //       weather: null,
+  //       error: "Error, please try again"
+  //     });
+  //   } else {
+  //     let weather = JSON.parse(body);
+  //     if (weather.main === undefined) {
+  //       res.render("weather", {
+  //         weather: null,
+  //         error: "Error, please try again"
+  //       });
+  //     } else {
+  //       let weatherText = weather.main.temp;
+  //       let city = weather.name;
+  //       let description = weather.weather[0].description;
+  //       res.render("weather", {
+  //         weather: { weatherText, city, description },
+  //         error: null
+  //       });
+  //     }
+  //   }
+  // });
 });
 
 module.exports = app;
